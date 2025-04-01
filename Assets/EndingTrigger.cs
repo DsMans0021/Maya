33d52af2cd31f5f9b35397603160a4e44873d8f1
ItsKareem <1022027.kareem@stemegypt.edu.eg>
2025-04-02 01:13:40 +0200

    polished level 1 and 2

---
+++ Assets/EndingTrigger.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ending2Trigger : MonoBehaviour
+{
+    public Animator Animator;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Animator.Play("Triggered");  // Play flag animation
+            Invoke("LoadNextScene", 1f); // Delay scene switch
+        }
+    }
+
+    void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+    }
+}
