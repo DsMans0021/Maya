dd97c272241f48c1a20976ad33e29e955bda2035
DsMans0021 <136956569+DsMans0021@users.noreply.github.com>
2025-04-03 21:14:57 +0200

    Fin

---
+++ Assets/Scripts/FallingSpikesCTRL.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallingSpikesCTRL : MonoBehaviour
+{
+    float wait = 0.2f;
+    public GameObject FallingSpikes;
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        InvokeRepeating(nameof(Spawn), wait, wait);
+    }
+    void OnDisable()
+    {
+        CancelInvoke(nameof(Spawn));
+    }
+
+    void Spawn()
+    {
+        Instantiate(FallingSpikes, new Vector3(Random.Range(-15f, 15f), 10, 0), Quaternion.identity);
+    }
+}
--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -7,7 +7,7 @@ public class Menu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene(2);
+        SceneManager.LoadScene(1);
     }
 
     public void QuitGame()
